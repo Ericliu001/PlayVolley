cdd132a31ea82e3e295197cc1c4e6d48b457f425
Eric Liu <eliu@nine.com.au>
2017-10-11 11:58:47 +1100

    setup RequestQueue.

---
+++ src/main/java/com/ericliu/RequestQueue.java
@@ -0,0 +1,27 @@
+package com.ericliu;
+
+import com.android.volley.Request;
+
+/**
+ * Created by ericliu on 11/10/17.
+ */
+
+public class RequestQueue {
+
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void cancelAll(final Object tag) {
+
+    }
+
+    public <T> void add(final Request<T> request) {
+        return;
+    }
+}
