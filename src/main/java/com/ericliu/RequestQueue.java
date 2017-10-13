b16fe074ce21699dcdff2b4c29af4a5ddc2a5285
Eric Liu <eliu@nine.com.au>
2017-10-13 11:11:56 +1100

    TDD RequestQueue

--- src/main/java/com/ericliu/RequestQueue.java
+++ src/main/java/com/ericliu/RequestQueue.java
@@ -1,6 +1,14 @@
 package com.ericliu;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue.RequestFilter;
+import com.android.volley.ResponseDelivery;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.PriorityBlockingQueue;
 
 /**
  * Created by ericliu on 11/10/17.
@@ -8,6 +16,24 @@ import com.android.volley.Request;
 
 public class RequestQueue {
 
+    /**
+     * The set of all requests currently being processed by this RequestQueue.
+     * A Request will be in this set if it is waiting in any queue or currently being processed
+     * by any dispatcher.
+     */
+    private final Set<Request<?>> mCurrentRequests = new HashSet<>();
+
+    /**
+     * The cache triage queue.
+     */
+    private final PriorityBlockingQueue<Request<?>> mCacheQueue = new PriorityBlockingQueue<>();
+
+    public RequestQueue(final Cache cache,
+                        final Network network,
+                        final int threadPoolSize,
+                        final ResponseDelivery delivery) {
+
+    }
 
     public void start() {
 
@@ -18,10 +44,23 @@ public class RequestQueue {
     }
 
     public void cancelAll(final Object tag) {
+        cancelAll(new RequestFilter() {
+            @Override
+            public boolean apply(final Request<?> request) {
+                return request.getTag() == tag;
+            }
+        });
+    }
 
+    public void cancelAll(final RequestFilter filter) {
+        for (final Request<?> request : mCurrentRequests) {
+            if (filter.apply(request)) {
+                request.cancel();
+            }
+        }
     }
 
     public <T> void add(final Request<T> request) {
-        return;
+        mCurrentRequests.add(request);
     }
 }
---
+++ src/test/java/com/ericliu/RequestQueueTest.java
@@ -0,0 +1,66 @@
+package com.ericliu;
+
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.ResponseDelivery;
+import com.android.volley.toolbox.NoCache;
+import com.ericliu.utils.ImmediateResponseDelivery;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowSystemClock;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * Created by ericliu on 13/10/17.
+ */
+
+@RunWith(RobolectricTestRunner.class)
+@Config(shadows = {ShadowSystemClock.class})
+public class RequestQueueTest {
+
+    private ResponseDelivery mDelivery;
+    @Mock
+    private Network mMockNetwork;
+
+    @Before
+    public void setUp() throws Exception {
+        mDelivery = new ImmediateResponseDelivery();
+        initMocks(this);
+    }
+
+    @Test
+    public void cancalAll_onlyCorrectTag() throws Exception {
+        RequestQueue queue = new RequestQueue(new NoCache(), mMockNetwork, 0, mDelivery);
+        Object tagA = new Object();
+        Object tagB = new Object();
+
+        Request req1 = mock(Request.class);
+        when(req1.getTag()).thenReturn(tagA);
+
+        Request req2 = mock(Request.class);
+        when(req2.getTag()).thenReturn(tagB);
+
+        Request req3 = mock(Request.class);
+        when(req3.getTag()).thenReturn(tagA);
+
+        Request req4 = mock(Request.class);
+        when(req4.getTag()).thenReturn(tagA);
+
+        queue.add(req1); // A
+        queue.add(req2); // B
+        queue.add(req3); // A
+        queue.cancelAll(tagA);
+        queue.add(req4); // A
+
+        verify(req1).cancel(); // A cancelled
+        verify(req3).cancel(); // B cancelled
+        verify(req2, never()).cancel(); // B not cancelled
+        verify(req4, never()).cancel(); // A added after cancel not cancelled
+    }
+}
