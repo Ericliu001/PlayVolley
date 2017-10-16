4ee1f21186bbf67a64a7a375a5c0000393a5485b
Eric Liu <eliu@nine.com.au>
2017-10-16 11:04:56 +1100

    setup RequestQueue.

--- src/test/java/com/ericliu/playvolley/RequestQueueIntegrationTest.java
+++ src/test/java/com/ericliu/playvolley/RequestQueueIntegrationTest.java
@@ -13,7 +13,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InOrder;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.robolectric.RobolectricTestRunner;
